d0eae2321f0f8b909306670651b183b85ce0fa5f
Jasman <jasmansingh84@gmail.com>
2019-10-26 23:57:25 -0400

    Feature : Define addition and subtraction

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,15 +1,24 @@
-def addition(a, b):
-    c = a + b
-    return c
+def addition(x, y):
+    z = x + y
+    return z
 
 
-def subtraction(a, b):
-    c = a - b
-    return c
+def subtraction(x, y):
+    z = x - y
+    return z
 
 
 class Calculator:
+    result = 0
 
 
     def __init__(self):
         pass
+
+    def add(self, x, y):
+        self.result = addition(x, y)
+        return self.result
+
+    def subtract(self, x, y):
+        self.result = subtraction(x, y)
+        return self.result

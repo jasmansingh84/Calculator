58d88e27bb26e8513ec0b7652e865d96cab03d12
Jasman <jasmansingh84@gmail.com>
2019-10-27 00:03:43 -0400

    Feature : Create Test cases and class MyTest

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -4,11 +4,20 @@ from Calculator import Calculator
 
 class MyTestCase(unittest.TestCase):
 
-
-    def test_instantiate_calculator(self):
-        calculator = Calculator()
-        self.assertIsInstance(calculator, Calculator)
+    def test_results_property(self):
+        self.assertEqual(self.calculator.result, 0)
 
 
-        if __name__ == '__main__':
-            unittest.main()
+    def test_addition(self):
+        self.assertEqual(self.calculator.add(1, 1), 2)
+        self.assertEqual(self.calculator.result, 2)
+
+
+    def test_subtraction(self):
+        self.assertEqual(self.calculator.subtract(1, 1), 0)
+        self.assertEqual(self.calculator.result, 0)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
